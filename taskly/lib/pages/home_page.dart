6e306b8ee78601830308565d802021e9957040a7
Alexcheto <a.chalakov@lancaster.ac.uk>
2022-06-20 16:39:56 +0300

    Tasks View, Task Lists, add Task Button and Popup display for button added

--- taskly/lib/pages/home_page.dart
+++ taskly/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomePage extends StatefulWidget {
   HomePage();
@@ -11,6 +12,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late double _deviceHeight, _deviceWidth;
+
+  String? _newTaskContent;
 
   _HomePageState();
 
@@ -28,6 +31,72 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+      body: _tasksView(),
+      floatingActionButton: _addTaskButton(),
     );
+  }
+
+  Widget _tasksView() {
+    return FutureBuilder(
+      future: Hive.openBox("tasks"),
+      builder: (BuildContext _context, AsyncSnapshot _snapshot) {
+        if (_snapshot.connectionState == ConnectionState.done) {
+          return _tasksList();
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _tasksList() {
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text(
+            "Do Laundry!",
+            style: TextStyle(
+              decoration: TextDecoration.lineThrough,
+            ),
+          ),
+          subtitle: Text(
+            DateTime.now().toString(),
+          ),
+          trailing: const Icon(
+            Icons.check_box_outlined,
+            color: Colors.red,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _addTaskButton() {
+    return FloatingActionButton(
+      onPressed: _displayTaskPopup,
+      child: const Icon(
+        Icons.add,
+      ),
+    );
+  }
+
+  void _displayTaskPopup() {
+    showDialog(
+        context: context,
+        builder: (BuildContext _context) {
+          return AlertDialog(
+            title: const Text("Add New Task!"),
+            content: TextField(
+              onSubmitted: (_value) {},
+              onChanged: (_value) {
+                setState(() {
+                  _newTaskContent = _value;
+                });
+              },
+            ),
+          );
+        });
   }
 }
